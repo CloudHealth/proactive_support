dcc4e6718605c14d3a7c9846fff48427d9cc6548
Steve Frank <lardcanoe@gmail.com>
2016-05-05 22:14:13 -0400

    View flags

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 ProactiveSupport::Engine.routes.draw do
   root to: 'proactive_support#index'
   resources :notes
+  resources :flags do
+    collection do
+      get '/:source/:identifier', to: 'flags#show'
+    end
+  end
 end
