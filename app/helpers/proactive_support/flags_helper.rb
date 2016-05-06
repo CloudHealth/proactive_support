7679cdb843c3018958ebf9e842c004179023208d
Steve Frank <lardcanoe@gmail.com>
2016-05-06 19:58:42 -0400

    Sort errors first

--- app/helpers/proactive_support/flags_helper.rb
+++ app/helpers/proactive_support/flags_helper.rb
@@ -5,13 +5,13 @@ module ProactiveSupport
       ar = ::ProactiveSupport::Flag.where(customer_id: customer_id, is_active: true)
       ar = ar.filter_transients if filter_transients
       ar.each do |f|
-        flags[f.source] ||= {}
-        flags[f.source][f.level] ||= {}
-        flags[f.source][f.level][f.identifier] ||= {message: f.message, count: 0, transients: 0}
-        flags[f.source][f.level][f.identifier][:count] += 1
-        flags[f.source][f.level][f.identifier][:transients] += 1 if f.is_transient
+        flags[f.level] ||= {}
+        flags[f.level][f.source] ||= {}
+        flags[f.level][f.source][f.identifier] ||= {message: f.message, count: 0, transients: 0}
+        flags[f.level][f.source][f.identifier][:count] += 1
+        flags[f.level][f.source][f.identifier][:transients] += 1 if f.is_transient
       end
-      flags
+      Hash[flags.sort.reverse] # errors first...
     end
   end
 end
