bfe4688202eace158c25a7e393ff3c86450ea476
Steve Frank <lardcanoe@gmail.com>
2016-06-01 10:19:13 -0400

    Fix namespace

---
+++ lib/proactive_support/adapters/alert.rb
@@ -0,0 +1,9 @@
+module ProactiveSupport
+  module Adapters
+    class Alert
+      def create(level, customer_id, message, options)
+
+      end
+    end
+  end
+end
--- lib/proactive_support/alert_adapter.rb
+++
@@ -1,7 +0,0 @@
-module ProactiveSupport
-  class AlertAdapter
-    def create(level, customer_id, message, options)
-
-    end
-  end
-end
