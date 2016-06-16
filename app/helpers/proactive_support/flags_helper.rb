46a19eb8caa05caa1c7c3bb333f7fe822b1724ce
Steve Frank <lardcanoe@gmail.com>
2016-06-15 22:36:11 -0400

    Get flags across all customers

--- app/helpers/proactive_support/flags_helper.rb
+++ app/helpers/proactive_support/flags_helper.rb
@@ -1,8 +1,9 @@
 module ProactiveSupport
   module FlagsHelper
-    def get_flag_groupings(customer_id, filter_transients = true)
+    def get_flag_groupings(customer_id = nil, filter_transients = true)
       flags = {}
-      ar = ::ProactiveSupport::Flag.where(customer_id: customer_id, is_active: true)
+      ar = ::ProactiveSupport::Flag.where(is_active: true)
+      ar = ar.where(customer_id: customer_id) if customer_id
       ar = ar.filter_transients if filter_transients
       ar.each do |f|
         flags[f.level] ||= {}
